d417d8ae7422b1455ff2b2f64078d1f4248d2e3d
SergioHumia <humiavelasco@gmail.com>
2022-01-18 01:40:55 +0000

    Casi final

--- resources/views/layouts/app4.blade.php
+++ resources/views/layouts/app4.blade.php
@@ -50,6 +50,7 @@
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        {!!QrCode::size(90)->generate(Request::url()) !!}
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
